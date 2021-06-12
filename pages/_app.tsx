b96b9bb246aa271639ed1d34f4e3a27bacc5467e
Petro Silenius <petro.silenius@gmail.com>
2021-06-12 19:41:38 +0300

    Add fonts to chakra theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import { AppProps } from 'next/app'
 import '../styles/globals.css'
+import { theme } from '../theme'
+import { ChakraProvider } from '@chakra-ui/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="container">
-      <Component {...pageProps} />
-    </div>
+    <ChakraProvider theme={theme}>
+      <div className="container">
+        <Component {...pageProps} />
+      </div>
+    </ChakraProvider>
   )
 }
 
