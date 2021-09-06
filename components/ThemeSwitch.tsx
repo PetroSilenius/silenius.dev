30ce3f726d40f9899e215085afa629f4d9355f87
Petro Silenius <petro.silenius@gmail.com>
2021-09-07 00:06:04 +0300

    Add back button to all but the index page

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -1,13 +1,23 @@
+import NextLink from 'next/link'
+import { useRouter } from 'next/router'
 import { Sun, Moon } from 'react-feather'
-import { IconButton, Icon, useColorMode } from '@chakra-ui/react'
+import { Button, IconButton, Icon, useColorMode } from '@chakra-ui/react'
 
 export const ThemeSwitch = (): JSX.Element => {
+  const { route } = useRouter()
   const { colorMode, toggleColorMode } = useColorMode()
 
   const IconComponent = colorMode === 'dark' ? Sun : Moon
 
   return (
     <aside>
+      {route !== '/' && (
+        <NextLink href="/">
+          <Button aria-label="Go back" position="absolute" top="5" left="5">
+            &larr;
+          </Button>
+        </NextLink>
+      )}
       <IconButton
         aria-label={`Toggle ${colorMode === 'dark' ? 'Light' : 'Dark'} mode`}
         icon={<Icon as={IconComponent} />}
