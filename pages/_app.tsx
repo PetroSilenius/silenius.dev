137c02a020f50bd1c72c32761f187fe2a0e56433
Petro Silenius <petro.silenius@gmail.com>
2021-09-13 21:20:10 +0300

    Default to dark color mode even on static/server rendered pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
 import { AppProps } from 'next/app'
 import { theme } from '../theme'
-import { Center, ChakraProvider } from '@chakra-ui/react'
+import { Center, ChakraProvider, ColorModeScript } from '@chakra-ui/react'
 import { ThemeSwitch } from '../components'
 
 function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode="dark" />
       <Center h="100vh" px="2" flexDirection="column">
         <ThemeSwitch />
         <Component {...pageProps} />
