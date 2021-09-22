a7d0251ba9d87a30c5f4252fd949e12039efc49c
Petro Silenius <petro.silenius@gmail.com>
2021-09-22 23:31:11 +0300

    Add title to resume iframe

--- pages/cv.tsx
+++ pages/cv.tsx
@@ -12,7 +12,12 @@ export default function Cv(): JSX.Element {
         />
       </Head>
       <Box height="100vh" width="100vw">
-        <iframe src="/Resume-Petro-Silenius.pdf" height="100%" width="100%">
+        <iframe
+          src="/Resume-Petro-Silenius.pdf"
+          title="Resume - Petro Silenius"
+          height="100%"
+          width="100%"
+        >
           This browser does not support PDFs. Please download the resume to view
           it:
           <a href="/Resume-Petro-Silenius.pdf">Download resume</a>
